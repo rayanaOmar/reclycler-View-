f446f5f473c301ac90f61a7b743cceeca1dbfcae
rayanaOmar <rayanaomar_@hotmail.com>
2021-09-28 11:39:11 +0300

    RecyclerViewApp

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -15,90 +15,33 @@ import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var clMain: ConstraintLayout
-    lateinit var guessFiled: EditText
-    lateinit var guessButton: Button
-    lateinit var messages: ArrayList<String>
-    lateinit var mGuess: TextView
-
-    var answer = 0
-    var guesses = 3
+    private lateinit var clroot: ConstraintLayout
+    private lateinit var guessField: EditText
+    private lateinit var guessButton: Button
+    private lateinit var messages: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        answer = Random.nextInt(10)
-
-        clMain = findViewById(R.id.clMain)
+        clroot = findViewById(R.id.clroot)
         messages = ArrayList()
-        mGuess = findViewById(R.id.mGuess)
 
-        rvMessages.adapter = MessageAdapter(this,messages)
-        rvMessages.layoutManager = LinearLayoutManager(this)
+        recyclerV.adapter = MessageAdapter(this, messages)
+        recyclerV.layoutManager = LinearLayoutManager(this)
 
-        guessFiled = findViewById(R.id.GuessField)
-        guessButton = findViewById(R.id.GuessButton)
+        guessField = findViewById(R.id.guessField)
+        guessButton = findViewById(R.id.guessButton)
 
-        guessButton.setOnClickListener { add() }
+        guessButton.setOnClickListener { addMessage() }
+
     }
-
-    fun add(){
-        val msg = guessFiled.text.toString()
+    private fun addMessage(){
+        val msg = guessField.text.toString()
         if(msg.isNotEmpty()){
-            if(guesses > 0 ){
-                if(msg.toInt() == answer){
-                    disableEntry()
-                    showAlertDialog("YOU WIN!!, PLAY AGAIN?")
-                }else{
-                    guesses--
-                    messages.add("YOU GUESSED $msg")
-                    messages.add("YOU HAVE $guesses GUESSES LEFT")
-                }
-                if(guesses == 0){
-                    disableEntry()
-                    messages.add("YOU ARE LOSE, THE CORRECT ANSWER WAS $answer")
-                    messages.add("GAME OVER")
-                    showAlertDialog("YOU ARE LOSE, THE CORRECT ANSWER WAS $answer")
-                }
-            }
-            guessFiled.text.clear()
-            guessFiled.clearFocus()
-            rvMessages.adapter?.notifyDataSetChanged()
-        }else{
-            Snackbar.make(clMain, "Please enter a number", Snackbar.LENGTH_LONG).show()
+            messages.add(msg)
+            guessField.text.clear()
+            guessField.clearFocus()
         }
     }
-    private fun disableEntry(){
-        guessButton.isEnabled = false
-        guessButton.isClickable = false
-        guessFiled.isEnabled = false
-        guessFiled.isClickable = false
-    }
-    private fun showAlertDialog(title: String) {
-        // build alert dialog
-        val dialogBuilder = AlertDialog.Builder(this)
-
-        // set message of alert dialog
-        dialogBuilder.setMessage(title)
-            // if the dialog is cancelable
-            .setCancelable(false)
-            // positive button text and action
-            .setPositiveButton("Yes", DialogInterface.OnClickListener {
-                    dialog, id -> this.recreate()
-            })
-            // negative button text and action
-            .setNegativeButton("No", DialogInterface.OnClickListener {
-                    dialog, id -> dialog.cancel()
-            })
-
-        // create dialog box
-        val alert = dialogBuilder.create()
-        // set title for alert dialog box
-        alert.setTitle("Game Over")
-        // show alert dialog
-        alert.show()
-    }
 }
-
-
